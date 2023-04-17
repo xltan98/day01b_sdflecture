0b7b0abe5b8ccc84b1cf845d2ab850eb30e41250
xltan98 <xinnli98@gmail.com>
2023-04-17 16:59:49 +0800

    slides todo

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,5 +1,6 @@
 package sg.edu.nus.iss;
-
+import java.io.Console;import java.util.List;
+import java.util.ArrayList;
 /**
  * Hello world!
  *
@@ -8,6 +9,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Console con= System.console();
+        List<String> todos = new ArrayList<String>();
+        String input ="";
+        while (input.equals("quit")){
+            input = con.readLine("Enter a string ");
+           if (!input.equals("quit")){
+            todos.add(input);
+
+        }
+        
     }
+
+    for (String todo:todos){
+        System.out.println("Todo task " +todo);
+    }
+}
 }
